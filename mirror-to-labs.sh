cbb05a53267a59e7aed339290f3c049a355b1034
Michael R. Lawrence <mike@michaellawrenceit.com>
2015-04-07 17:23:05 -0400

    resync/package mooves from comm overlay

--- mirror-to-labs.sh
+++ mirror-to-labs.sh
@@ -1,8 +1,10 @@
 ### This is set-up for making a public layman for testing by non spike-  general Gentoo use Abuse and Etc..
 ### you can always edit this to keep your own fork In sync, 
-git remote set-url --push origin https://github.com/Sabayon-Labs/Spike-Rise.git
-git remote add origin push origin https://github.com/Sabayon-Labs/Spike-Rise.git
+git remote set-url --push origin https://github.com/Sabayon-Labs/spike-overlay-testing.git
+git remote add origin https://github.com/Sabayon-Labs/spike-overlay-testing.git
+git remote add upstream https://github.com/Spike-Pentesting/spike-overlay.git
 git checkout master
+git fetch upstream
 #git merge upstream/master
 git merge master
 #git rebase upstream/master
